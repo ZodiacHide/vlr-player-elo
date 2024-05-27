08e36d61978bac88ca15800f0f331b231d34518a
ZodiacHide <simen1708@gmail.com>
2024-05-28 01:34:41 +0200

    Update file

--- fetch_all_player_names.py
+++ fetch_all_player_names.py
@@ -3,6 +3,7 @@ from bs4 import BeautifulSoup
 import bs4
 import time
 import numpy as np
+from data_struct import get_user_input_on_scraping
 
 def connect(url: str) -> tuple:
     response = requests.get(url)
@@ -13,7 +14,7 @@ def connect(url: str) -> tuple:
             content = response.content
             return True, content
         except:
-            return False, None 
+            return False, 0 
         
 def parse_html(content: str) -> bs4.BeautifulSoup:
     parsed = BeautifulSoup(content, 'html.parser')
@@ -23,7 +24,7 @@ def check_for_team(parsed_html: bs4.BeautifulSoup) -> tuple:
     team_container = parsed_html.find_all('a', attrs={'style': 'display: flex; justify-content: space-between; align-items: center; padding: 15px 20px;'})
     if len(team_container) == 0:
         # Player has no summary, we're not interested
-        return False, None
+        return False, 0
     
     team_count = len(team_container)
     player_teams = np.zeros(team_count, dtype=object)
@@ -39,16 +40,16 @@ def get_player_name(parsed_html: bs4.BeautifulSoup) -> tuple:
     player_alias = parsed_html.find('h1')
     if player_alias == None:
         # Unable to find player_alias
-        return False, None
+        return False, 0
     try:
         player_name = player_alias.find_next('h2')
     except:
         # Unable to find player alias or name
-        return False, None
+        return False, 0
     
     if player_name == None:
         # No name, not interested
-        return False, None 
+        return False, 0 
 
     alias = ' '.join(player_alias.get_text().split())
     name = ' '.join(player_name.get_text().split())
@@ -68,11 +69,11 @@ def write_to_file(player_id: int, player_alias: str, player_name: str, team_list
                 infile.write(f'Unable write player: {_player_alias}: {player_id}\n')
         pass   
     
-def scraper(player_count: int):
-    for id in range(1, player_count+1):
+def scraper(start_id: int, player_count: int):
+    for id in range(start_id, start_id + player_count+1):
         # Avoid overloading
         time.sleep(1)
-        print(id)
+        print(f'Player id: {id}')
         url = f"https://www.vlr.gg/player/{id}"
         answer, content = connect(url=url)
         if not answer:
@@ -111,10 +112,16 @@ def scraper(player_count: int):
 
         write_to_file(player_id=id, player_alias=player_names[0], 
                       player_name=player_names[1], team_list=player_team)
+    
+    return player_count + start_id
         
 
-def main():
-    scraper(5)
+def main(begin: int=1):
+    player_count = get_user_input_on_scraping()
+    if player_count == None:
+        exit()
+    new_start = scraper(149, player_count)
+    main(new_start)
 
 if __name__=='__main__':
     main()
