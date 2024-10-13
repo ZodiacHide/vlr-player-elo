ba0b780413006fa4f1752875baee989bd104b312
ZodiacHide <simen1708@gmail.com>
2024-10-14 00:41:44 +0200

    Add method to delete db row

--- src/tools/tools.py
+++ src/tools/tools.py
@@ -1,4 +1,5 @@
 import os
+import sqlite3
 from typing import Optional
 
 def find_data_directory(start_dir: Optional[str] = os.path.dirname(os.path.abspath(__file__))) -> str:
@@ -17,3 +18,21 @@ def find_data_directory(start_dir: Optional[str] = os.path.dirname(os.path.abspa
             raise FileNotFoundError("The data directory was not found in the directory tree.")
         
         current_dir = parent_dir
+
+def delete_row(table_name:str, index_name:str, index_value):
+    if not isinstance(table_name, str):
+        raise TypeError(f"'table_name' must be of type 'str', but got {type(table_name).__name__}")
+    if not isinstance(index_name, str):
+        raise TypeError(f"'index_name' must be of type 'str', but got {type(index_name).__name__}")
+    # Path to the db
+    db_path = os.path.join(find_data_directory(), 'valorant.db')
+    try:
+        # Connect to the SQLite database
+        conn = sqlite3.connect(db_path)
+        cursor = conn.cursor()
+        cursor.execute(f"DELETE FROM {table_name} WHERE {index_name} = ?", (index_value,))
+        conn.commit()
+    finally:
+        # Close the connection
+        if conn:
+            conn.close()
