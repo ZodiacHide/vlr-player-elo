2f9e03f745eae05254e0a9b76e57d711b20ed91d
ZodiacHide <simen1708@gmail.com>
2024-10-13 15:43:17 +0200

    Add more methods

--- src/vlr_player_elo/getters_db.py
+++ src/vlr_player_elo/getters_db.py
@@ -1,7 +1,7 @@
 import sqlite3
 
 # Function to fetch player data by ID
-def get_player(player_id):
+def get_player(player_id:int) -> tuple:
     conn = sqlite3.connect('valorant.db')
     cursor = conn.cursor()
     cursor.execute('SELECT * FROM players WHERE player_id = ?', (player_id,))
@@ -9,11 +9,38 @@ def get_player(player_id):
     conn.close()
     return player
 
+# Function to fetch team data by ID
+def get_team(team_id:int) -> tuple:
+    conn = sqlite3.connect('valorant.db')
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM teams WHERE team_id = ?', (team_id,))
+    team = cursor.fetchone()
+    conn.close()
+    return team
+
 # Function to fetch series data by ID
-def get_series(series_id):
+def get_series(series_id:int) -> tuple:
     conn = sqlite3.connect('valorant.db')
     cursor = conn.cursor()
     cursor.execute('SELECT * FROM series WHERE series_id = ?', (series_id,))
     series = cursor.fetchone()
     conn.close()
     return series
+
+# Function to fetch map data by ID
+def get_map(map_id:int) -> tuple:
+    conn = sqlite3.connect('valorant.db')
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM maps WHERE map_id = ?', (map_id,))
+    map = cursor.fetchone()
+    conn.close()
+    return map
+
+# Function to fetch player performance data by ID
+def get_player_performance(performance_id:int) -> tuple:
+    conn = sqlite3.connect('valorant.db')
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM player_performances WHERE performance_id = ?', (performance_id,))
+    performance = cursor.fetchone()
+    conn.close()
+    return performance
