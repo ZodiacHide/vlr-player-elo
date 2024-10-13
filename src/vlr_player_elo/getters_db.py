c687f748d11ff3f8ec1c1368fa8f6126a1755d1e
ZodiacHide <simen1708@gmail.com>
2024-10-13 21:05:32 +0200

    Update filepaths and error handling

--- src/vlr_player_elo/getters_db.py
+++ src/vlr_player_elo/getters_db.py
@@ -1,4 +1,6 @@
 import sqlite3
+import os
+from tools.tools import find_data_directory
 
 ###############
 # PLAYER DATA #
@@ -6,7 +8,10 @@ import sqlite3
 
 # Function to fetch player data by ID
 def get_player(player_id:int) -> tuple:
-    conn = sqlite3.connect('valorant.db')
+    # Path to the db
+    db_path = os.path.join(find_data_directory(), 'valorant.db')
+
+    conn = sqlite3.connect(db_path)
     cursor = conn.cursor()
     cursor.execute('''SELECT * FROM players 
                    WHERE player_id = ?''', (player_id,))
@@ -20,7 +25,57 @@ def get_player(player_id:int) -> tuple:
 
 # Function to fetch team data by team ID
 def get_team_by_team_id(team_id:int) -> tuple:
-    conn = sqlite3.connect('valorant.db')
+    """
+    ## Returns
+        **team** : *tuple*
+        Tuple containing datapoints for team_id:
+
+        ### Identification:
+        - 0  : *int*    : team_id
+        - 1  : *str*    : team_name
+
+        ### Roster:
+        - 2  : *str*    : current_roster
+        - 3  : *str*    : previous_players
+
+        ### Match Statistics:
+        - 4  : *int*    : maps_played
+        - 5  : *int*    : maps_won
+        - 6  : *int*    : series_played
+        - 7  : *int*    : series_won
+        - 8  : *int*    : rounds_played
+        - 9  : *int*    : defence_rounds_won
+        - 10 : *float*  : defence_winp
+        - 11 : *int*    : offence_rounds_won
+        - 12 : *float*  : offence_winp
+
+        ### Map Specific Statistics:
+        - 13 : *int*    : abyss_played
+        - 14 : *int*    : abyss_won
+        - 15 : *int*    : ascent_played
+        - 16 : *int*    : ascent_won
+        - 17 : *int*    : bind_played
+        - 18 : *int*    : bind_won
+        - 19 : *int*    : breeze_played
+        - 20 : *int*    : breeze_won
+        - 21 : *int*    : fracture_played
+        - 22 : *int*    : fracture_won
+        - 23 : *int*    : haven_played
+        - 24 : *int*    : haven_won
+        - 25 : *int*    : icebox_played
+        - 26 : *int*    : icebox_won
+        - 27 : *int*    : lotus_played
+        - 28 : *int*    : lotus_won
+        - 29 : *int*    : pearl_played
+        - 30 : *int*    : pearl_won
+        - 31 : *int*    : split_played
+        - 32 : *int*    : split_won
+        - 33 : *int*    : sunset_played
+        - 34 : *int*    : sunset_won"""
+    # Path to the db
+    db_path = os.path.join(find_data_directory(), 'valorant.db')
+
+    conn = sqlite3.connect(db_path)
     cursor = conn.cursor()
     cursor.execute('''SELECT * FROM teams 
                    WHERE team_id = ?''', (team_id,))
@@ -34,7 +89,10 @@ def get_team_by_team_id(team_id:int) -> tuple:
 
 # Function to fetch series data by series ID
 def get_series_by_series_id(series_id:int) -> tuple:
-    conn = sqlite3.connect('valorant.db')
+    # Path to the db
+    db_path = os.path.join(find_data_directory(), 'valorant.db')
+
+    conn = sqlite3.connect(db_path)
     cursor = conn.cursor()
     cursor.execute('''SELECT * FROM series 
                    WHERE series_id = ?''', (series_id,))
@@ -44,7 +102,10 @@ def get_series_by_series_id(series_id:int) -> tuple:
 
 # Function to fetch series data by team ID
 def get_series_by_team_id(team_id:int) -> tuple:
-    conn = sqlite3.connect('valorant.db')
+    # Path to the db
+    db_path = os.path.join(find_data_directory(), 'valorant.db')
+
+    conn = sqlite3.connect(db_path)
     cursor = conn.cursor()
     cursor.execute('''SELECT * FROM series WHERE ? IN 
                    (team1_id, team2_id)''', (team_id,))
@@ -54,7 +115,10 @@ def get_series_by_team_id(team_id:int) -> tuple:
 
 # Function to fetch series data by team ID
 def get_series_by_map_id(map_id:int) -> tuple:
-    conn = sqlite3.connect('valorant.db')
+    # Path to the db
+    db_path = os.path.join(find_data_directory(), 'valorant.db')
+
+    conn = sqlite3.connect(db_path)
     cursor = conn.cursor()
     cursor.execute('''SELECT * FROM series WHERE ? IN 
                    (map1_id, map2_id, map3_id, map4_id, 
@@ -69,7 +133,10 @@ def get_series_by_map_id(map_id:int) -> tuple:
 
 # Function to fetch map data by map ID
 def get_map_by_map_id(map_id:int) -> tuple:
-    conn = sqlite3.connect('valorant.db')
+    # Path to the db
+    db_path = os.path.join(find_data_directory(), 'valorant.db')
+
+    conn = sqlite3.connect(db_path)
     cursor = conn.cursor()
     cursor.execute('''SELECT * FROM maps 
                    WHERE map_id = ?''', (map_id,))
@@ -79,7 +146,10 @@ def get_map_by_map_id(map_id:int) -> tuple:
 
 # Function to fetch map data by series ID
 def get_map_by_series_id(series_id:int) -> tuple:
-    conn = sqlite3.connect('valorant.db')
+    # Path to the db
+    db_path = os.path.join(find_data_directory(), 'valorant.db')
+
+    conn = sqlite3.connect(db_path)
     cursor = conn.cursor()
     cursor.execute('''SELECT * FROM maps 
                    WHERE series_id = ?''', (series_id,))
@@ -89,7 +159,10 @@ def get_map_by_series_id(series_id:int) -> tuple:
 
 # Function to fetch map data by series ID
 def get_map_by_team_id(team_id:int) -> tuple:
-    conn = sqlite3.connect('valorant.db')
+    # Path to the db
+    db_path = os.path.join(find_data_directory(), 'valorant.db')
+
+    conn = sqlite3.connect(db_path)
     cursor = conn.cursor()
     cursor.execute('''SELECT * FROM maps WHERE ? IN 
                    (team1_id, team2_id)''', (team_id,))
@@ -103,7 +176,10 @@ def get_map_by_team_id(team_id:int) -> tuple:
 
 # Function to fetch player performance data by ID
 def get_player_performance_by_performance_id(performance_id:int) -> tuple:
-    conn = sqlite3.connect('valorant.db')
+    # Path to the db
+    db_path = os.path.join(find_data_directory(), 'valorant.db')
+
+    conn = sqlite3.connect(db_path)
     cursor = conn.cursor()
     cursor.execute('''SELECT * FROM player_performances 
                    WHERE performance_id = ?''', (performance_id,))
@@ -113,7 +189,10 @@ def get_player_performance_by_performance_id(performance_id:int) -> tuple:
 
 # Function to fetch player performance data by player ID
 def get_player_performance_by_player_id(player_id:int) -> tuple:
-    conn = sqlite3.connect('valorant.db')
+    # Path to the db
+    db_path = os.path.join(find_data_directory(), 'valorant.db')
+
+    conn = sqlite3.connect(db_path)
     cursor = conn.cursor()
     cursor.execute('''SELECT * FROM player_performances 
                    WHERE player_id = ?''', (player_id,))
@@ -123,7 +202,10 @@ def get_player_performance_by_player_id(player_id:int) -> tuple:
 
 # Function to fetch player performance data by map ID
 def get_player_performance_by_map_id(map_id:int) -> tuple:
-    conn = sqlite3.connect('valorant.db')
+    # Path to the db
+    db_path = os.path.join(find_data_directory(), 'valorant.db')
+
+    conn = sqlite3.connect(db_path)
     cursor = conn.cursor()
     cursor.execute('''SELECT * FROM player_performances 
                    WHERE map_id = ?''', (map_id,))
@@ -133,7 +215,10 @@ def get_player_performance_by_map_id(map_id:int) -> tuple:
 
 # Function to fetch player performance data by team ID
 def get_player_performance_by_team_id(team_id:int) -> tuple:
-    conn = sqlite3.connect('valorant.db')
+    # Path to the db
+    db_path = os.path.join(find_data_directory(), 'valorant.db')
+
+    conn = sqlite3.connect(db_path)
     cursor = conn.cursor()
     cursor.execute('''SELECT * FROM player_performances 
                    WHERE team_id = ?''', (team_id,))
--- src/vlr_player_elo/setters_db.py
+++ src/vlr_player_elo/setters_db.py
@@ -1,13 +1,21 @@
 import sqlite3
+import os
+from tools.tools import find_data_directory
 
 # Function to insert a player into the database
 def insert_player(player_id:int, alias:str, country:str, name:str) -> None:
-    conn = sqlite3.connect('valorant.db')
+    # Path to the db
+    db_path = os.path.join(find_data_directory(), 'valorant.db')
+
+    conn = sqlite3.connect(db_path)
     cursor = conn.cursor()
-    cursor.execute('''
-    INSERT INTO players (player_id, alias, country, name)
-    VALUES (?, ?, ?, ?)
-    ''', (player_id, alias, country, name))
+    try:
+        cursor.execute('''
+        INSERT INTO players (player_id, alias, country, name)
+        VALUES (?, ?, ?, ?)
+        ''', (player_id, alias, country, name))
+    except sqlite3.IntegrityError:
+        print(f"player_id : {player_id}, already exists.")
     conn.commit()
     conn.close()
 
@@ -19,24 +27,30 @@ def insert_team(team_id:int, team_name:str, current_roster:str, previous_players
                   fracture_played:int, fracture_won:int, haven_played:int, haven_won:int, icebox_played:int, icebox_won:int,
                   lotus_played:int, lotus_won:int, pearl_played:int, pearl_won:int, split_played:int, split_won:int,
                   sunset_played:int, sunset_won:int) -> None:
-    conn = sqlite3.connect('valorant.db')
+    # Path to the db
+    db_path = os.path.join(find_data_directory(), 'valorant.db')
+
+    conn = sqlite3.connect(db_path)
     cursor = conn.cursor()
-    cursor.execute('''
-    INSERT INTO teams (team_id, team_name, current_roster, previous_players, maps_played,
-                  maps_won, series_played, series_won, rounds_played, defence_rounds_won,
-                  defence_winp, offence_rounds_won, offence_winp, abyss_played, abyss_won,
-                  ascent_played, ascent_won, bind_played, bind_won, breeze_played, breeze_won,
-                  fracture_played, fracture_won, haven_played, haven_won, icebox_played, icebox_won,
-                  lotus_played, lotus_won, pearl_played, pearl_won, split_played, split_won,
-                  sunset_played, sunset_won)
-    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
-    ''', (team_id, team_name, current_roster, previous_players, maps_played,
-                  maps_won, series_played, series_won, rounds_played, defence_rounds_won,
-                  defence_winp, offence_rounds_won, offence_winp, abyss_played, abyss_won,
-                  ascent_played, ascent_won, bind_played, bind_won, breeze_played, breeze_won,
-                  fracture_played, fracture_won, haven_played, haven_won, icebox_played, icebox_won,
-                  lotus_played, lotus_won, pearl_played, pearl_won, split_played, split_won,
-                  sunset_played, sunset_won))
+    try:
+        cursor.execute('''
+        INSERT INTO teams (team_id, team_name, current_roster, previous_players, maps_played,
+                    maps_won, series_played, series_won, rounds_played, defence_rounds_won,
+                    defence_winp, offence_rounds_won, offence_winp, abyss_played, abyss_won,
+                    ascent_played, ascent_won, bind_played, bind_won, breeze_played, breeze_won,
+                    fracture_played, fracture_won, haven_played, haven_won, icebox_played, icebox_won,
+                    lotus_played, lotus_won, pearl_played, pearl_won, split_played, split_won,
+                    sunset_played, sunset_won)
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+        ''', (team_id, team_name, current_roster, previous_players, maps_played,
+                    maps_won, series_played, series_won, rounds_played, defence_rounds_won,
+                    defence_winp, offence_rounds_won, offence_winp, abyss_played, abyss_won,
+                    ascent_played, ascent_won, bind_played, bind_won, breeze_played, breeze_won,
+                    fracture_played, fracture_won, haven_played, haven_won, icebox_played, icebox_won,
+                    lotus_played, lotus_won, pearl_played, pearl_won, split_played, split_won,
+                    sunset_played, sunset_won))
+    except sqlite3.IntegrityError:
+        print(f"team_id : {team_id}, already exists.")
     conn.commit()
     conn.close()
 
@@ -44,15 +58,21 @@ def insert_team(team_id:int, team_name:str, current_roster:str, previous_players
 def insert_series(series_id:int, team1_id:int, team2_id:int, event_name:str, date_played:str, time_started:str,
                   series_format:str, team1_score:int, team2_score:int, map1_id:int, map2_id:int, map3_id:int,
                   map4_id:int, map5_id:int) -> None:
-    conn = sqlite3.connect('valorant.db')
+    # Path to the db
+    db_path = os.path.join(find_data_directory(), 'valorant.db')
+
+    conn = sqlite3.connect(db_path)
     cursor = conn.cursor()
-    cursor.execute('''
-    INSERT INTO series (series_id, team1_id, team2_id, event_name, date_played, time_started,
-                   series_format, team1_score, team2_score, map1_id, map2_id, map3_id, map4_id,
-                   map5_id)
-    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
-    ''', (series_id, team1_id, team2_id, event_name, date_played, time_started,
-          series_format, team1_score, team2_score, map1_id, map2_id, map3_id, map4_id, map5_id))
+    try:
+        cursor.execute('''
+        INSERT INTO series (series_id, team1_id, team2_id, event_name, date_played, time_started,
+                    series_format, team1_score, team2_score, map1_id, map2_id, map3_id, map4_id,
+                    map5_id)
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+        ''', (series_id, team1_id, team2_id, event_name, date_played, time_started,
+            series_format, team1_score, team2_score, map1_id, map2_id, map3_id, map4_id, map5_id))
+    except sqlite3.IntegrityError:
+        print(f"series_id : {series_id}, already exists.")
     conn.commit()
     conn.close()
 
@@ -62,29 +82,41 @@ def insert_series(series_id:int, team1_id:int, team2_id:int, event_name:str, dat
 def insert_map(map_id:int, series_id:int, map_name:str, picked_by:str, team1_id:int, team1_fh_score:int,
                team1_sh_score:int, team2_id:int, team2_fh_score:int, team2_sh_score:int, pistol_fh_winner:str,
                pistol_sh_winner:str, vod_link:str, map_length:str) -> None:
-    conn = sqlite3.connect('valorant.db')
+    # Path to the db
+    db_path = os.path.join(find_data_directory(), 'valorant.db')
+
+    conn = sqlite3.connect(db_path)
     cursor = conn.cursor()
-    cursor.execute('''
-    INSERT INTO maps (map_id, series_id, map_name, picked_by, team1_id, team1_fh_score,
-               team1_sh_score, team2_id, team2_fh_score, team2_sh_score, pistol_fh_winner,
-               pistol_sh_winner, vod_link, map_length)
-    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
-    ''', (map_id, series_id, map_name, picked_by, team1_id, team1_fh_score,
-               team1_sh_score, team2_id, team2_fh_score, team2_sh_score, pistol_fh_winner,
-               pistol_sh_winner, vod_link, map_length))
+    try:
+        cursor.execute('''
+        INSERT INTO maps (map_id, series_id, map_name, picked_by, team1_id, team1_fh_score,
+                team1_sh_score, team2_id, team2_fh_score, team2_sh_score, pistol_fh_winner,
+                pistol_sh_winner, vod_link, map_length)
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+        ''', (map_id, series_id, map_name, picked_by, team1_id, team1_fh_score,
+                team1_sh_score, team2_id, team2_fh_score, team2_sh_score, pistol_fh_winner,
+                pistol_sh_winner, vod_link, map_length))
+    except sqlite3.IntegrityError:
+        print(f"map_id : {map_id}, already exists.")
     conn.commit()
     conn.close()
 
 # Function to insert a player performance into the database
-def insert_player_performance(performance_id:int, player_id:int, map_id:int, team_id:int, rating:float, acs:int,
+def insert_player_performance(player_id:int, map_id:int, team_id:int, rating:float, acs:int,
                               kills:int, deaths:int, assists:int, kast:float, adr:float, hsp:float, fk:int, fd:int) -> None:
-    conn = sqlite3.connect('valorant.db')
+    # Path to the db
+    db_path = os.path.join(find_data_directory(), 'valorant.db')
+
+    conn = sqlite3.connect(db_path)
     cursor = conn.cursor()
-    cursor.execute('''
-    INSERT INTO player_performances (performance_id, player_id, map_id, team_id, rating, acs,
-                kills, deaths, assists, kast, adr, hsp, fk, fd)
-    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
-    ''', (performance_id, player_id, map_id, team_id, rating, acs,
-                kills, deaths, assists, kast, adr, hsp, fk, fd))
+    try: 
+        cursor.execute('''
+        INSERT INTO player_performances (performance_id, player_id, map_id, team_id, rating, acs,
+                    kills, deaths, assists, kast, adr, hsp, fk, fd)
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+        ''', (player_id, map_id, team_id, rating, acs, kills, 
+            deaths, assists, kast, adr, hsp, fk, fd))
+    except sqlite3.IntegrityError:
+        print(f"An error occurred.")
     conn.commit()
     conn.close()
