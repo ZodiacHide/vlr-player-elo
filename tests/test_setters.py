445f0faa7e4869b01f7dbc2290d531aa9c533f2c
ZodiacHide <simen1708@gmail.com>
2024-10-14 02:42:47 +0200

    Update file

--- tests/test_setters.py
+++ tests/test_setters.py
@@ -12,7 +12,7 @@ from tools.tools import delete_row
 def test_insert_player_no_error_for_same_id():
     player_id = int(1337+1e10)
     filler_count = len(inspect.signature(setters_db.insert_player).parameters)-2
-    vars = [player_id] + [None]*filler_count
+    vars = [player_id] + ['']*filler_count
 
     setters_db.insert_player(*vars, test=True)
     setters_db.insert_player(*vars, test=True)
@@ -22,7 +22,7 @@ def test_insert_player_no_error_for_same_id():
 def test_insert_team_no_error_for_same_id():
     team_id = int(1337+1e10)
     filler_count = len(inspect.signature(setters_db.insert_team).parameters)-2
-    vars = [team_id] + [None]*filler_count
+    vars = [team_id] + ['']*filler_count
 
     setters_db.insert_team(*vars, test=True)
     setters_db.insert_team(*vars, test=True)
@@ -31,8 +31,10 @@ def test_insert_team_no_error_for_same_id():
 
 def test_insert_series_no_error_for_same_id():
     series_id = int(1337+1e10)
-    filler_count = len(inspect.signature(setters_db.insert_series).parameters)-2
-    vars = [series_id] + [None]*filler_count
+    team1_id = int(1338+1e10)
+    team2_id = int(1339+1e10)
+    filler_count = len(inspect.signature(setters_db.insert_series).parameters)-2-6
+    vars = [series_id, team1_id, team2_id] + ['']*4 + [0]*filler_count
     
     setters_db.insert_series(*vars, test=True)
     setters_db.insert_series(*vars, test=True)
@@ -41,8 +43,10 @@ def test_insert_series_no_error_for_same_id():
 
 def test_insert_map_no_error_for_same_id():
     map_id = int(1337+1e10)
-    filler_count = len(inspect.signature(setters_db.insert_map).parameters)-2
-    vars = [map_id] + [None]*filler_count
+    series_id = int(1337+1e10)
+    map_name = ''
+    filler_count = len(inspect.signature(setters_db.insert_map).parameters)-2-4
+    vars = [map_id, series_id, map_name] + [0]*filler_count + ['']*2
     
     setters_db.insert_map(*vars, test=True)
     setters_db.insert_map(*vars, test=True)
